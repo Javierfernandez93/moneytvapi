e9fd782e5d7adf9a85fc3911659a42baf6642701
Javier Fernández <leqjl93@hotmail.com>
2023-05-16 18:26:54 -0600

    Update controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -6,7 +6,7 @@ const DEFAULT_PACKAGE = 2
 
 const init = async function () {
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: false,
     defaultViewport: null,
     executablePath: await chromium.executablePath,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
